2c2f9a3d34899001de36936f0d55eb02314f321e
CodingAcc12 <124944276+CodingAcc12@users.noreply.github.com>
2023-09-21 19:25:03 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var canvas = new fabric.Canvas('myCanvas');
+var x= document.getElementById("myAudio");
+
+function player_update(){
+    fabric.Image.fromURL('BirthdayImage.jpg',function(img){
+        player_object=img;
+        player_object.scaleToWidth(700);
+        player_object.scaleToHeight(510);
+        player_object.set({
+            top:0,
+            left:0
+        })
+        canvas.add(player_object)
+    })
+}
+
+function playsound(){
+x.play()
+}
